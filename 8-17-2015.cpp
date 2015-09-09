9ca802371df5b01076c240b2fd6b335296195e2e
Zoilo Mercedes <zmercedes89@gmail.com>
2015-09-09 13:30:58 -0400

    first commit

---
+++ 8-17-2015.cpp
@@ -0,0 +1,33 @@
+/**
+ Zoilo Mercedes
+ Daily programming challenge, 8-17-2015
+ Letters in alphabetical order
+ Checks to see if letts in word are in alphabetical order
+ */
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool ordered(string word){
+	for(int i = 0; i < word.length() - 1;i++){
+		if(word[i] >= word[i++])
+			return false;
+	}
+	return true;
+}
+
+int main(){
+	string word;
+
+	while(cin >> word){
+		if(ordered(word))
+			cout << word << " IN ORDER\n";
+		else
+			cout<< word << " NOT IN ORDER\n";
+	}
+	return 0
+}
+/**
+ Note: <algorithm> has methods for checking if things are in alph order. (is_sorted())
+ */
---
+++ 9-7-2015.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Convert(string row){
+  string tmp;
+  for(int i = 0;i<row.length();i++){
+    tmp+= (row[i]=='0' ? " " : "x");
+  }
+  return tmp;
+}
+
+int main(){
+  string row;
+  cout<< "### Cellular Automata: Rule 90 ### \n Starting with: ";
+  cin >> row;
+
+  int i,count = 0;
+  string nextRow = Convert(row);
+
+  cout << nextRow << endl;
+
+  while(count < 500){
+    nextRow = "";
+    for(i=0;i<row.length();i++){
+      if(i==0)
+        nextRow += (row[1]=='x' ? "x":" ");
+      else if(i == row.length()-1)
+        nextRow += (row[i-1]=='x' ? "x":" ");
+      else if(row[i-1] != row[i+1])
+        nextRow += "x";
+      else
+        nextRow += " ";
+    }
+    cout << nextRow << endl;
+    row = nextRow;
+    count++;
+  }
+  cout << "\n Complete!";
+
+  return 0;
+}
